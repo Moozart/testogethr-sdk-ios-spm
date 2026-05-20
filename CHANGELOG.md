5a1dd709f6b5c4ea5e940bfc75b2b86aee86b509
Moozart - Yns <ynskeskn@gmail.com>
2026-05-20 17:04:09 +0300

    release(spm): prepare 0.1.0 package manifest and checksum

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,9 @@
 
 ## [Unreleased]
 
-- Initial SwiftPM distribution repository structure.
-- Added binary package manifest template.
+- No changes yet.
+
+## [0.1.0] - 2026-05-20
+
+- First public SwiftPM binary package release.
+- Added `TestogethrSdk` binary target metadata and checksum.
--- Package.swift
+++ Package.swift
@@ -12,9 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TestogethrSdk",
-            // Replace URL and checksum on each release.
             url: "https://github.com/Moozart/testogethr-sdk-ios-spm/releases/download/v0.1.0/TestogethrSdk.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            checksum: "da2ff8771fd8bb16a253965da6b67833076b7bb4b83c662660e2f5d3de89b801"
         )
     ]
 )
